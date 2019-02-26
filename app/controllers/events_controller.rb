f22f9386806c3e39c7a2690c13472650a2cba1f9
ukroma1992 <ukroma1992@gmail.com>
2019-02-26 17:15:53 +0200

    users initial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,20 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
   def index
     @events = Event.all
   end
 
-  # GET /events/1
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
   def create
     @event = Event.new(event_params)
 
@@ -30,7 +25,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
   def update
     if @event.update(event_params)
       redirect_to @event, notice: 'Event was successfully updated.'
@@ -39,20 +33,18 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
   def destroy
     @event.destroy
     redirect_to events_url, notice: 'Event was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def event_params
-      params.require(:event).permit(:title, :address, :datetime, :description)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :address, :datetime, :description)
+  end
 end
