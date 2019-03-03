3322cfacb726ad1cf0cad5196dabb82019f7ef8f
ukroma1992 <ukroma1992@gmail.com>
2019-03-03 19:04:48 +0200

    configure devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :current_user_can_edit?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:password, :password_confirmation, :current_password)
+    end
+  end
+
+  def current_user_can_edit?(event)  
+    user_signed_in? && event.user == current_user
+  end
 end
