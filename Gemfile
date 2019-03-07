0e57d026240290f51195c2f336c2759e3c60fcd5
ukroma1992 <ukroma1992@gmail.com>
2019-03-07 12:18:15 +0200

    Add carrierwave and avatar to user

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem "jquery-rails"
 gem 'devise'
 gem 'devise-i18n'
 gem "russian"
+gem 'carrierwave', '~> 1.0'
+gem 'rmagick'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   def user_avatar(user)
-    asset_path('user.png') 
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_path('user.png')
+    end 
   end
 
   def fa_icon(icon_class) 
